6eed477e99e667299914d7492bf3da16df5fe032
Dilshan Fardil <rishiedni@gmail.com>
2017-08-01 15:48:15 +0530

    Code the Studenet DTO for the testing part Connection factory is included

--- dao/ConnectionFactory.js
+++ dao/ConnectionFactory.js
@@ -1,3 +1,24 @@
 /**
  * Created by Dilshan on 7/27/2017.
  */
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'1234',
+    database:'iStat'
+});
+
+connection.connect(function (err) {
+    if(err){
+        console.log(err.message);
+        return;
+    }
+    console.log("Connection has been successfully established...");
+});
+
+exports.getConnection = function () {
+    return connection;
+}
